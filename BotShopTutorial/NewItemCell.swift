6235abbd28165357f16b9476480d064ea7c9df94
Deer5000 <khidrbrinkley32@gmail.com>
2021-03-04 22:13:37 -0500

    Set up basic colection view

---
+++ BotShopTutorial/NewItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  NewItemCell.swift
+//  BotShopTutorial
+//
+//  Created by Khidr Brinkley on 3/4/21.
+//
+
+import Foundation
+import UIKit
+
+class NewItemCell: UICollectionViewCell {
+    
+}
+
+
