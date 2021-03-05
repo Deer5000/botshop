c3ec87d5b0cefb2d7615dfff1b5319dfd67dfd75
Deer5000 <khidrbrinkley32@gmail.com>
2021-03-04 21:19:59 -0500

    first commit

---
+++ BotShopTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BotShopTutorial
+//
+//  Created by Khidr Brinkley on 3/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
