551227058ead96cec2be9ef9457a51a41902b75b
Deer5000 <khidrbrinkley32@gmail.com>
2021-03-04 22:27:13 -0500

    Users can see items for sale in collection view

--- BotShopTutorial/NewOrderViewController.swift
+++ BotShopTutorial/NewOrderViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class NewOrderViewController: UIViewController {
+    let data = [
+        Item(title: "Respiratory", image: UIImage(named: "robot1")!),
+        Item(title: "Muscular", image: UIImage(named: "robot2")!),
+        Item(title: "Endocrine", image: UIImage(named: "robot3")!),
+        Item(title: "Excretory", image: UIImage(named: "robot4")!),
+        Item(title: "Lymphatic", image: UIImage(named: "robot5")!),
+        Item(title: "Nervous", image: UIImage(named: "robot6")!)
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,15 +51,16 @@ class NewOrderViewController: UIViewController {
 
 extension NewOrderViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return data.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! NewItemCell
         cell.backgroundColor = .green
+        cell.data = self.data[indexPath.row]
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width/2, height: collectionView.frame.height/4)
+        return CGSize(width: collectionView.frame.width/1.8, height: collectionView.frame.height/2)
     }
 }
---
+++ Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  BotShopTutorial
+//
+//  Created by Khidr Brinkley on 3/4/21.
+//
+
+import Foundation
+import UIKit
+
+struct Item {
+    var title: String
+    var image: UIImage
+}
